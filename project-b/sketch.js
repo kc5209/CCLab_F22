c6bd81332b557be1de1be92f3c86d0ef65b93a30
Kelly Chow <kc5209@users.noreply.github.com>
2022-12-13 13:23:16 +0800

    Update sketch.js

--- project-b/sketch.js
+++ project-b/sketch.js
@@ -8,7 +8,7 @@ let buildings = []; // empty;
 let cvs;
 
 function setup() {
-    cvs = createCanvas(900, 500);
+    cvs = createCanvas(1200, 500);
     cvs.parent("canvasContainer");
     background(200, 40);
     angleMode(DEGREES);
